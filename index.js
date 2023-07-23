db50e40afecf0e89b989e69f1c975978ae9a494c
Peris Wakio <peris.kingori@student.moringaschool.com>
2023-07-23 08:32:40 -0700

    completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = () => 2000/100;
+console.log(divide())
+
+const square = (x) => x*x 
+console.log(square(4))
+
+const add = (x,y) => x + y
+const result = add(2,4);
+
+console.log(result)
